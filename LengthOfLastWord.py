a25e44b45491cdda1263624bd2d08521f62154e5
axeld <axel.dovskog@outlook.com>
2025-01-21 10:49:05 +0100

    Misc2

--- LengthOfLastWord.py
+++ LengthOfLastWord.py
@@ -1,4 +1,3 @@
 def lengthOfLastWord(self, s: str) -> int:
-    splitted = s.split(" ")
-    parsed = list(filter(lambda w: w != " ", splitted))
-    return len(parsed[-1])
+    splitted = s.strip().split(" ")
+    return len(splitted[-1])
