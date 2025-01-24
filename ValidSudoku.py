5239f18595c54fa0bfa72facdb86f5a13f913fa6
axeld <axel.dovskog@outlook.com>
2025-01-24 11:42:04 +0100

    Valid sudoku

---
+++ ValidSudoku.py
@@ -0,0 +1,25 @@
+def isValidSudoku(self, board: List[List[str]]) -> bool:
+    
+    subBoxes = {} # Key: "{row // 3}-{col // 3}", value: set of digits
+    rows = {} # Key: row idx, value: set of digits
+    cols = {} # Key: col idx, value: set of digits
+    
+    for row in range(len(board)):
+        if row not in rows.keys(): rows[row] = set([])
+        
+        for col in range(len(board[row])):
+            if col not in cols.keys(): cols[col] = set([])
+            
+            val = board[row][col]
+            
+            if val in rows[row] or val in cols[col] or val > 9 or val < 0: return False
+            
+            subBox = f"{row // 3}-{col // 3}"
+            if subBox not in subBoxes.keys(): subBoxes[subBox] = set([])
+            if val in subBoxes[subBox]: return False
+            
+            rows[row].add(val)
+            cols[col].add(val)
+            subBoxes[subBox].add(val)
+            
+    return True
