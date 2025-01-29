8af6d4f45209167a0fd100fdcd8fcc670d970f16
axeld <axel.dovskog@outlook.com>
2025-01-29 12:59:03 +0100

    Working on DirectorOfPhotographyII

--- DirectorOfPhotography2.py
+++ DirectorOfPhotography2.py
@@ -1,44 +1,34 @@
 def getArtisticPhotographCount(N: int, C: str, X: int, Y: int) -> int:
     res = 0
     
-    for i in range(len(C)):
-        current = C[i]
+    for i in range(2):
+        numPhotographers = 0
+        for c in C[X:Y]:
+            if c == "P": numPhotographers += 1
+        numBackgrounds = 0
+        for c in C[Y+X:2*Y]:
+            if c == "B": numBackgrounds += 1
         
-        if current == "A":
-            leftWindow = C[max(0, i-Y):max(i-X+1,0)]
-            rightWindow = C[min(i+X, len(C)):min(i+Y+1,len(C))]
+        for t, c in enumerate(C[Y:]):
+            if t + 2*Y < len(C) and C[t + 2*Y] == "B": numBackgrounds += 1
             
-            numPhotographers = 0
-            numBackgrounds = 0
-            for el in leftWindow:
-                if el == "P": numPhotographers += 1
-            for el in rightWindow:
-                if el == "B": numBackgrounds += 1
+            if c == ".": continue
+            
+            if c == "A":
+                res += numPhotographers * numBackgrounds
+            if C[Y+t-X] == "P": numPhotographers += 1
+            if c == "B": numBackgrounds -= 1
                 
-            res += numPhotographers*numBackgrounds
-           
-    C = list(reversed(C)) 
-    for i in range(len(C)):
-        current = C[i]
+            if C[t] == "P": numPhotographers -= 1
+            
+        C = list(reversed(C))
         
-        if current == "A":
-            leftWindow = C[max(0, i-Y):max(i-X+1,0)]
-            rightWindow = C[min(i+X, len(C)):min(i+Y+1,len(C))]
-            
-            numPhotographers = 0
-            numBackgrounds = 0
-            for el in leftWindow:
-                if el == "P": numPhotographers += 1
-            for el in rightWindow:
-                if el == "B": numBackgrounds += 1
-                
-            res += numPhotographers*numBackgrounds
-    
+        
     return res
 
 
-N = 5
-C = "APABA"
-X = 2
+N = 8
+C = ".PBAAP.B"
+X = 1
 Y = 3
 print(getArtisticPhotographCount(N,C,X,Y))
