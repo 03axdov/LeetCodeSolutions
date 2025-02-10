a7bd3ada1e0d392b2b59a722a6df1dc3a09ecc7e
axeld <axel.dovskog@outlook.com>
2025-02-10 19:01:01 +0100

    Misc

--- MissingMail.py
+++ MissingMail.py
@@ -1,38 +1,33 @@
 from typing import List
+# Write any import statements here
 
 
-dic = {}
 def getMaxExpectedProfit(N: int, V: List[int], C: int, S: float) -> float:
-    global dic
-    dic = {}
-    res = getMaxExpectedProfitHelper(N, V, C, S, 0)
-    return res
-
-
-def getMaxExpectedProfitHelper(N: int, V: List[int], C: int, S: float, mailRoom, startI=0) -> float:
-    global dic
-    alts = []
+    mailRoom = 0
     cash = 0
     
     for i in range(N):
         mailRoom += V[i]
         
-        if i + startI not in dic.keys():
-            print(i + startI)
-            dic[i + startI] = getMaxExpectedProfitHelper(N - i - 1, V[i+1:], C, S, mailRoom * (1 - S), i + startI+1)  # If we don't take
-            
-        alts.append(cash + dic[i + startI])
-        
-        cash += mailRoom - C
-        mailRoom = 0
+        countUnworth = 1
+        for j in range(i+1,N):
+            if C >= S * V[j]:
+                countUnworth += 1
+            else:
+                break
     
-    print(alts)    
+        if C < S * countUnworth * mailRoom or (i == N - 1 and C < mailRoom):
+            print(f"i: {i}, countUnworth: {countUnworth}")
+            cash += mailRoom - C
+            mailRoom = 0
+            
+        mailRoom = mailRoom * (1 - S)
+    
+    return cash
 
-    dic[startI] = max([cash, max(alts, default=0)])
-    return dic[startI]
 
 N = 5
 V = [10, 2, 8, 6, 4]
 C = 3
 S = 0.15
-print(getMaxExpectedProfit(N, V, C, S))
+print(getMaxExpectedProfit(N,V,C,S))
