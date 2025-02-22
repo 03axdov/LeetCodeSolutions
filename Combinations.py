c26811f273220f2457b22478b38272dcc2e37176
axeld <axel.dovskog@outlook.com>
2025-02-22 09:53:10 +0100

    Combinations.py

---
+++ Combinations.py
@@ -0,0 +1,22 @@
+from typing import List
+
+def combine(self, n: int, k: int) -> List[List[int]]:
+    res = []
+    comb = []
+
+    def backtrack(start):
+        if len(comb) == k:
+            res.append(comb[:])
+            return
+        
+        for num in range(start, n + 1):
+            comb.append(num)
+            backtrack(num + 1)
+            comb.pop()
+
+    backtrack(1)
+    return res
+
+
+
+print(combine(4, 2))
