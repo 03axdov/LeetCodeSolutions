497ea188a2b3359577aa3f8e1880179817d08378
axeld <axel.dovskog@outlook.com>
2025-03-05 16:47:07 +0100

    ProductOfArrayExceptSelfO(n).ts

---
+++ ProductOfArrayExceptSelfO(n).ts
@@ -0,0 +1,17 @@
+function productExceptSelf(nums: number[]): number[] {
+    let trailing: Map<number, number> = new Map();
+    let current: number = 1;
+    for (let i=nums.length - 1; i >= 0; i--) {
+        trailing.set(i, current);
+        current *= nums[i];
+    }
+
+    let res: number[] = [];
+    current = 1;
+    for (let i=0; i < nums.length; i++) {
+        res.push(current * trailing.get(i)!);
+        current *= nums[i];
+    }
+
+    return res;
+};
