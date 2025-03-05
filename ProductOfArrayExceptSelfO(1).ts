ab511fb8e93c90cff33d0f1676ee4a6e406aaec6
axeld <axel.dovskog@outlook.com>
2025-03-05 16:58:53 +0100

    O(1) space solution to prev

--- ProductOfArrayExceptSelfO(1).ts
+++ ProductOfArrayExceptSelfO(1).ts
@@ -1,15 +1,16 @@
-function productExceptSelfOn(nums: number[]): number[] {
-    let trailing: Map<number, number> = new Map();
+function productExceptSelf(nums: number[]): number[] {
+    let res: number[] = Array(nums.length).fill(0);
+
     let current: number = 1;
     for (let i=nums.length - 1; i >= 0; i--) {
-        trailing.set(i, current);
+        res[i] = current;
         current *= nums[i];
     }
 
-    let res: number[] = [];
+
     current = 1;
     for (let i=0; i < nums.length; i++) {
-        res.push(current * trailing.get(i)!);
+        res[i] *= current;
         current *= nums[i];
     }
 
